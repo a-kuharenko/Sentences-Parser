8f9eefc51ca2a02eeeffcde3a53113ee94a2de71
AlexanderKuharenko <Sandrk27@gmail.com>
2018-10-08 20:20:54 +0300

    Bug fixes

--- sentencesParser.js
+++ sentencesParser.js
@@ -6,11 +6,13 @@ const cheerio = require('cheerio');
 const input = process.stdin;
 
 input.on('data', (data) => {
+
   let url = 'http://sentence.yourdictionary.com/';
   url += data;
   needle.get(url, (err, res) => {
 
     const $ = cheerio.load(res.body);
+
     let sentences = $('.li_content').text();
 
     sentences = sentences.replace('Mr.', 'Mr')
@@ -21,11 +23,13 @@ input.on('data', (data) => {
 
     let counter = 0;
     for (let i = 0; i < 30; i++) {
+      if (sentences.length - 1 === i)
+        break;
       if (!sentences[i].includes('\"')) {
         counter++;
         console.log(counter + '. ' + sentences[i] + '.');
-      } else
-        i += 1;
+      } else i += 1;
     }
   });
 });
+
