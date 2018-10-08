8089c6c9f10f90c5154c613c8a93e7a5602fca5a
AlexanderKuharenko <Sandrk27@gmail.com>
2018-10-08 19:17:11 +0300

    Release

---
+++ sentencesParser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const needle = require('needle');
+const cheerio = require('cheerio');
+
+const input = process.stdin;
+
+input.on('data', (data) => {
+  let url = 'http://sentence.yourdictionary.com/';
+  url += data;
+  needle.get(url, (err, res) => {
+
+    const $ = cheerio.load(res.body);
+    let sentences = $('.li_content').text();
+
+    sentences = sentences.replace('Mr.', 'Mr')
+      .replace('Mrs.', 'Mrs')
+      .replace('?', '.')
+      .replace('!', '.')
+      .split('.');
+
+    let counter = 0;
+    for (let i = 0; i < 30; i++) {
+      if (!sentences[i].includes('\"')) {
+        counter++;
+        console.log(counter + '. ' + sentences[i] + '.');
+      } else
+        i += 1;
+    }
+  });
+});
